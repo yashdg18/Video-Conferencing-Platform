04250f79ee9ba8c5558d6435d7d2c813192da78d
yashdg18 <yashdg18@gmail.com>
2026-03-28 01:08:34 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://video-conferencing-platform-9.onrender.com" :
+    "https://video-conferencing-platform-1-xdo8.onrender.com" :
 
     "http://localhost:8000"
 
