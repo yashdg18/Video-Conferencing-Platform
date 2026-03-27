b9478b0eafe72aee67e8b3c60f6a7e495f46b3e7
yashdg18 <yashdg18@gmail.com>
2026-03-27 12:16:53 +0530

    Update server URL for production environment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://video-conferencing-platform-1-xdo8.onrender.com" :
 
     "http://localhost:8000"
 
